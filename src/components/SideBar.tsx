b43d43e3981e9a14a412b63dc15dc68c98c3f05e
Mariana Stefany <marianastefanycaio@gmail.com>
2022-04-18 13:59:34 -0300

    Desafio finalizado

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,30 @@
-export function SideBar() {
-  // Complete aqui
+import { GenreResponseProps } from "../App";
+import { Button } from "./Button";
+
+interface Props {
+  genres: GenreResponseProps[];
+  selectGenre: (id: number) => void;
+  selectedGenreId: number;
+}
+
+export function SideBar({ genres, selectGenre, selectedGenreId }: Props) {
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => selectGenre(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
